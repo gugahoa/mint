584ff8ca7e7bed2d2dbc6ae83ada49475b27a39b
Gustavo Aguiar <gugahoa@gmail.com>
2020-11-14 10:20:54 -0300

    flesh out client api

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,39 @@
+use std::marker::PhantomData;
+
+struct RobotFetched;
+struct RobotUnfetched;
+
+pub struct Client<T> {
+    _marker: PhantomData<T>,
+    client: String,
+    host: String,
+}
+
+impl Client<RobotUnfetched> {
+    pub fn new(host: String) -> Self {
+        Client {
+            client: "".into(),
+            host,
+            _marker: PhantomData,
+        }
+    }
+
+    pub async fn fetch_robots(self) -> Client<RobotFetched> {
+        Client {
+            client: self.client,
+            host: self.host,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl Client<RobotFetched> {
+    pub async fn get(&self, path: String) -> Result<String, String> {
+        dbg!(&self.client);
+        Ok(self.host.clone() + &path)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
